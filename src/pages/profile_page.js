919f3d9e09cb0499192360b20fe058eb47253d34
Tweek-Tweak <61298009+Tweek-Tweak@users.noreply.github.com>
2021-04-01 23:12:10 -0600

    Update profile_page.js

--- src/pages/profile_page.js
+++ src/pages/profile_page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Image, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, Text, View, TouchableOpacity } from 'react-native';
 
 export default function ProfilePage(){
     return(
